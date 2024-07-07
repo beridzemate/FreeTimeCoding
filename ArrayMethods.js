f66b37d45063b4d6a57a5de822758caacd3d4e71
Vovaadidas777 <mateberidze464@gmail.com>
2024-07-07 23:05:12 +0400

    uptade commit

---
+++ ArrayMethods.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3];
+arr.push(4); 
+console.log(arr)
+
+
+
+
+let arr1 = ["mate", "nata", "ninia"]
+arr1.push("matexa");
+console.log(arr1);
+
+
+let arr2 = (["mate", "lizi", "elene"])
+arr2.push("matexa1")
+console.log(arr2)
+
+
